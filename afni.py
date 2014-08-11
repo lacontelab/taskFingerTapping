fadcf45c2485767dd48d77337f4f695bb6e207a9
Jonathan Lisinski <jonathan.lisinski@gmail.com>
2014-08-11 12:01:38 -0400

    Some modifications before sending to Scott

---
+++ afni.py
@@ -0,0 +1,128 @@
+import sys
+import socket
+
+# use this default IP address if the IP address can not be determined using
+#   hostnames
+AFNI_DEFAULT_HOST_IP='127.0.0.1'
+
+# Instead of using separate y-axis (subplots) for each plot UNIte them 
+#   using only one y-axis (currently only supported by our version of AFNI, 
+#   added by Jeff Zemla) 
+AFNI_GRAPH_UNI=1
+
+# set GEOMetry of AFNIi plots
+AFNI_PLOT_GEOM='1100x400+850+680'
+
+def getAfniHostIP():
+
+  # get hostname of stimulus computer
+  try: 
+    hostname = socket.gethostname()
+  except: 
+    hostname = 'undefined'
+ 
+  # map AFNI host (TABS) to stimulus computer 
+  # R1081
+  if 'd1l-0000j2n9nn1' in hostname.lower():
+    try:
+      afni_host_ip=socket.gethostbyname('tabs-mr1081')
+    except:
+      afni_host_ip=AFNI_DEFAULT_HOST_IP
+
+  # R1082
+  elif 'd1l-0000j2nbnn1' in hostname.lower():
+    try:
+      afni_host_ip=socket.gethostbyname('tabs-mr1082')
+    except:
+      afni_host_ip=AFNI_DEFAULT_HOST_IP
+
+  # CRC
+  elif 'crc-mri-byhzcp1' in hostname.lower():
+    try:
+      afni_host_ip=socket.gethostbyname('tabs-mr1052')
+    except:
+      afni_host_ip=AFNI_DEFAULT_HOST_IP
+    
+  else:
+    afni_host_ip=AFNI_DEFAULT_HOST_IP
+    
+  return afni_host_ip
+
+'''
+Could someday write a general updatePlugPlot that takes arrays... But for now...
+'''
+def updatePlugPlot5 (ff, name, x, y1, y2, y3, y4, y5):
+    try:
+      print >> ff, "ADDTO_GRAPH_XY %s %f %f %f %f %f %f" %(str(name), 
+          float(x), float(y1), float(y2), float(y3), float(y4), float(y5))
+    except :
+      print "Warning: Updating plugout plot failed!"
+      raise
+
+    ff.flush()
+
+def updatePlugPlot4 (ff, name, x, y1, y2, y3, y4):
+    try:
+      print >> ff, "ADDTO_GRAPH_XY %s %f %f %f %f %f" %(str(name), 
+          float(x), float(y1), float(y2), float(y3), float(y4))
+    except :
+      print "Warning: Updating plugout plot failed!"
+      raise
+
+    ff.flush()
+
+def updatePlugPlot3 (ff, name, x, y1, y2, y3):
+    try:
+      print >> ff, "ADDTO_GRAPH_XY %s %f %f %f %f" %(str(name), 
+          float(x), float(y1), float(y2), float(y3))
+    except :
+      print "Warning: Updating plugout plot failed!"
+      raise
+
+    ff.flush()
+
+def updatePlugPlot2 (ff, name, x, y1, y2):
+    try:
+      print >> ff, "ADDTO_GRAPH_XY %s %f %f %f" %(str(name), 
+          float(x), float(y1), float(y2))
+    except :
+      print "Warning: Updating plugout plot failed!"
+      raise
+
+    ff.flush()
+
+def updatePlugPlot1 (ff, name, x, y1):
+    try:
+      print >> ff, "ADDTO_GRAPH_XY %s %f %f" %(str(name), 
+          float(x), float(y1))
+    except :
+      print "Warning: Updating plugout plot failed!"
+      raise
+
+    ff.flush()
+
+def closePlugPlot(ff, name):
+    try:
+      print >> ff, "CLOSE_GRAPH_XY %s" %str(name)
+    except:
+      print "Warning: closing plugout plot failed!"
+      raise
+
+    ff.flush()
+
+def openPlugPlot(ff, name, title, y_num, x_min, x_max, x_label, y_min, y_max, y_label, y_names):
+    try:
+      print >>ff, "SETENV AFNI_graph_uni %d" %AFNI_GRAPH_UNI
+      print >>ff, "OPEN_GRAPH_XY %s '%s' %f %f '%s' %f %f %f '%s' %s" %(str(name), \
+      str(title), float(x_min), float(x_max), str(x_label), float(y_num), float(y_min), float(y_max), str(y_label), str(y_names) )
+      print >>ff, "SET_GRAPH_GEOM %s geom=%s" %(name, AFNI_PLOT_GEOM)
+    except:
+      print "Warning: opening plugout plot failed!"
+      raise
+    
+    ff.flush()
+
+if __name__ == '__main__':
+   print '** this is just a collection of afni utility functions and not a main module'
+   sys.exit(1)
+
--- motor_testing.py
+++ motor_testing.py
@@ -4,27 +4,17 @@
 # Flags and defines         #
 #############################
 
-DBG           = 1    # Set 1 to print info/debugging statements
-TRN           = 0    # Set 1 for training and 0 for testing
-WINDOWS       = 0    # Set 1 on Windows and 0 on Linux/Mac
+TCPIP         = 1     # Set 1 to receive data over TCP/IP
+TCPIP_PORT    = 8000  # Port receiving TCP/IP data
+TCPIP_GUI     = 0     # Open GUI to initialize TCP/IP 
+TCPIP_SIM     = 0     # Simulate TCPIP data by using data from LABEL_FILE
 
-TCPIP         = 1    # Set 1 to receive data over TCP/IP
-TCPIP_PORT    = 8000 # Port receiving TCP/IP data
-TCPIP_GUI     = 1   # Open GUI to initialize TCP/IP 
-
-AFNI_PLUGOUT  = 0   #Set 1 to enable afni plugout plots
-AFNI_HOST_IP  = 'localhost' # host running AFNI receiving plugout_drive commands
-
-SERIAL        = 0   #Set 1 to receive data over serial port
-BOUT_RATE     = 300 #Set bout rate for serial communication 
-
-TR_DUR        = 2 # TR duration in seconds, number of TRs read from first line in LABEL_FILE
-LABEL_FILE    = 'lbls_vegg_motor_tst_afniDemo.dat' # label file controlling presentation
-IMAGE_DIR     = 'images'   # name of directory in ../. containing images for presentation 
-IMAGE_SIZE    = (800,600)  # resolution of images in IMAGE_DIR
+TR_DUR        = 2     # TR duration in seconds
+IMAGE_SIZE    = (800,600)  # resolution of stimulus images
 SCREEN_SIZE   = (1024,768) # screen size 
-LOG_PREFIX    = 'log_motor_tst_afniDemo_' # prefix for log file
+LABEL_FILE    = 'labels_test.dat' # label file controlling presentation
 
+AFNI_PLUGOUT  = 1     # Set 1 to enable afni plugout plots
 
 ############################
 #  Import various modules  #
@@ -40,15 +30,13 @@ from    VisionEgg.Text import *
 from    VisionEgg.Textures import *
 from    VisionEgg.ResponseControl import *
 from    math import *
-from    scipy import polyfit
+from    numpy import polyfit
 import  pygame
 import  OpenGL.GL as gl
 from    string import *
 import  Image, ImageDraw # Python Imaging Library (PIL)
 import  os, sys
-
-if SERIAL:
-  import serial
+import afni
 
 if TCPIP:
   from VisionEgg.TCPController import *
@@ -72,44 +60,20 @@ VisionEgg.start_default_logging();
 VisionEgg.watch_exceptions();
 
 ### read files, directories, etc. 
-base_dir = os.getcwd()
-log_file_name = time.strftime ('%m-%d-%Y_%Hh-%Mm.txt');
+time_str = time.strftime ('%m-%d-%Y_%Hh-%Mm');
 prog_name = split (os.path.basename (sys.argv[0]), '.')
-log_file_name = str (LOG_PREFIX) + log_file_name
-
-if WINDOWS:
-  tmp_dir = base_dir + str ('\..')
-  img_dir = '\\'.join([tmp_dir, IMAGE_DIR,'']);
-else: 
-  tmp_dir = base_dir + str ('/..')
-  img_dir ='/'.join([tmp_dir, IMAGE_DIR, ''])
-
-if DBG:
-  print '\n============================ Initialization ==========================' 
-  print ' Label file:            %s' % (LABEL_FILE)
-  print ' Log file:              %s' % (log_file_name)
-  print ' Current directory :    %s' % (base_dir)
-  print ' Directory with images: %s' % (IMAGE_DIR )
-  print '========================================================================\n'
+log_file_name = 'logfile_' + prog_name[0] + '_' + time_str + '.log'
 
 # open label and log file
-if DBG: print 'Opening files  ',
 label_file=open(LABEL_FILE, 'r')
 log_file=open(log_file_name,'w')
-if DBG: print '...done.'
 
 # initialize log file
-if TRN:
-  log_file.write("# LOGFILE: %s, %s (TRAINING)\n" %(prog_name[0], time.strftime ('%m-%d-%Y %H:%M')))
-else:
-  log_file.write("# LOGFILE: %s, %s (TESTING)\n" %(prog_name[0], time.strftime ('%m-%d-%Y %H:%M')))
-
 log_file.write("# time;currBlock;currShow;dist;dist_detrend;fbScore;fbScale;l_count;r_count\n")
 
 # some variable definitions     
 TR_n = int(label_file.readline())
 sec_n = TR_n*TR_DUR
-current_block = 0
 
 # define arrays read from label file
 block_array     = []
@@ -120,8 +84,6 @@ stimImg_names   = []
 fb_array        = []
 
 # read label file
-if DBG: print 'Reading label file ',
-
 for line in label_file:
     b, s, i, fb, xxx = line.split(";", 4)
     block_array = block_array + [b]
@@ -130,20 +92,14 @@ for line in label_file:
     fb_array = fb_array + [fb]
 
 label_file.close
-if DBG: print '...done.'
 
 #print '\nblock: %s' % (block_array)
 #print 'show: %s' % (show_array)
 #print 'img names: %s' % (stimImg_names),
 
-if DBG: print 'Loading images ',
-img_name='%sright.png' %(img_dir)
-txtr_posFb = Texture(img_name)
-img_name='%sleft.png' %(img_dir)
-txtr_negFb = Texture(img_name)
-img_name='%sframe2.png' %(img_dir)
-txtr_frame = Texture(img_name)
-if DBG: print '...done.\n'
+txtr_posFb = Texture('right.png')
+txtr_negFb = Texture('left.png')
+txtr_frame = Texture('frame.png')
 
 # Determine block lengths 
 nbl=0
@@ -173,37 +129,20 @@ for ntr in range(TR_n):
     b = block_lengths[nbl]
     block_len_array.append(b)
 
-if SERIAL:
-  try:
-    ser = serial.Serial(0,BOUT_RATE,timeout=0,parity=serial.PARITY_NONE,rtscts=1)
-    #ser = serial.Serial(0,9600,timeout=0,parity=serial.PARITY_EVEN,rtscts=1)
-  except:
-    sys.stderr.write("ERROR: Could not open serial port\n\n")
-    sys.exit()
-
-# Initialize plots of button presses using AFNIs plugout_drive
+# try establishing connection to afni for plotting button presses
+afni_plugout_connected=1
 if AFNI_PLUGOUT:
-  # AFNI plugout functions
-  def updatePlugPlot (name, x_pos, n_left, n_right): 
-    global ff
-    print >> ff, "ADDTO_GRAPH_XY %s %5.2f %d %d" % (name, float(x_pos), int(n_left), int(n_right))
+  afni_host_ip=afni.getAfniHostIP()
+  plugout_command ='plugout_drive -port 8100 -host %s' % afni_host_ip
+  ff=os.popen(plugout_command, 'w') 
+else:
+  afni_plugout_connected=0
 
-  def closePlugPlot(name):
-    print >> ff, "CLOSE_GRAPH_XY %s" %name
-
-  def openPlugPlot(name, title, x_max, y_max):
-    global ff
-
-    print >>ff, "OPEN_GRAPH_XY %s '%s' 0 %d 'measurements (TRs)' 2 0 %d presses left right " %(name, title, x_max, y_max) 
-    print >>ff, "SET_GRAPH_GEOM %s geom=1000x400+920+775" %name
-
-
-  plugout_command ='plugout_drive -port 8100 -host %s' % AFNI_HOST_IP
-  ff=os.popen(plugout_command, 'w')
-  # close if already open 
-  closePlugPlot('buttons')
-  openPlugPlot('buttons', 'button presses', TR_n, 20)
-
+# initialize plot with button presses
+if afni_plugout_connected:
+  afni.closePlugPlot(ff, 'plot_0')
+  afni.openPlugPlot(ff, 'plot_0', 'button presses', 2, 0, TR_n, 'TRs', 0, 20, 'presses per TR', "left right")
+ 
 #################################
 #  Initialize the various bits  #
 #################################
@@ -218,19 +157,24 @@ screen.parameters.bgcolor = (0.0,0.0,0.0,0.0)
 screen_half_x = screen.size[0]/2
 screen_half_y = screen.size[1]/2
 
+if TCPIP: 
+  text_instruct3_str='WITH FEEDBACK'
+else:
+  text_instruct3_str=''
+
 text_instruct_1 = Text(text='',
             color=(1.0,1.0,1.0),
             position=(screen_half_x,screen_half_y+120),
 	        font_size=40,
 	        anchor='center')
 
-text_instruct_2 = Text(text='FEEDBACK',
+text_instruct_2 = Text(text='FINGER TAPPING',
             color=(1.0,1.0,1.0),
             position=(screen_half_x,screen_half_y+120),
 	        font_size=80,
 	        anchor='center')
 
-text_instruct_3 = Text(text='FINGER TAPPING',
+text_instruct_3 = Text(text=text_instruct3_str,
             color=(1.0,1.0,1.0),
             position=(screen_half_x,screen_half_y+30),
 	        font_size=80,
@@ -305,7 +249,7 @@ prev_bar_pos = bar_pos_min
 next_bar_pos = bar_pos_min
 curr_bar_pos = bar_pos_min
 
-img_name='%s%s' %(img_dir,stimImg_names[0])
+img_name=str(stimImg_names[0])
 currStimTxtr = Texture(img_name)
 posFbVis = 0
 posFbTxtr = txtr_posFb
@@ -327,25 +271,26 @@ time_array = [];
 startDetrend = 0
 dist=0;
 dist_detrend=0;
+bar_blend_time = 0.8
+bar_step = 0
+currBlockLen = 1
 
 #################################
 # Main state function           #
 #################################
 
 def getState(t):
-    global TR_DUR, next_TR_time, prev_TR_time, currBlock,currShow 
-    global first_loop, start_time, TRcount, currStimTxtr 
+    global next_TR_time, prev_TR_time, TRcount
+    global first_loop, start_time, currStimTxtr 
     global bar_pos, bar_pos_max, bar_pos_min, prev_bar_pos, curr_bar_pos, next_bar_pos
+    global bar_step, currBlockLen
     global posFbVis, negFbVis, barVis, frameVis 
     global negFbTxtr, posFbTxtr
     global prevBlock, fbScore, fbScale
-    global l_count, r_count
     global tcp_listner, startDetrend, dist_array, time_array, dist, dist_detrend
 
-    bar_step = 0
-    currBlockLen = 1
-    blend_time = 0.8
-    
+    global l_count, r_count
+
     if (first_loop == 1) & (p.parameters.trigger_go_if_armed) :
       first_loop = 0
       start_time = VisionEgg.time_func()
@@ -361,7 +306,7 @@ def getState(t):
 
       currBlock = int(block_array[TRcount])
       currShow = int(show_array[TRcount])
-      img_name='%s%s' %(img_dir,stimImg_names[TRcount])
+      img_name=str(stimImg_names[TRcount])
       currStimTxtr = Texture(img_name)
       currBlockLen = int(block_len_array[TRcount])
       bar_step = (bar_pos_max)/float(currBlockLen)
@@ -426,46 +371,52 @@ def getState(t):
       if (currBlock != 0) and ((currShow == 1) or (currShow == 1111)):
         fbScore = fbScore + (int(fbScale) - 5)/4.0*bar_step
       
+
+      if afni_plugout_connected:
+        afni.updatePlugPlot2(ff, 'plot_0', TRcount, l_count, r_count)
+
       log_file.write("%5.3f;%d;%d;%5.3f;%5.3f;%5.3f;%d;%d;%d\n" % (t, int(currBlock), int(currShow), float(dist), float(dist_detrend), float(fbScore), int(fbScale), int(l_count), int(r_count)) )
       
-      if AFNI_PLUGOUT == 1:
-        updatePlugPlot('buttons', TRcount, l_count, r_count)
-
+      # rest counter for left and right button presses
       l_count = 0
       r_count = 0
       
-    if TRN:
-        barVis = 0
-        frameVis = 1
+    if not TCPIP and not TCPIP_SIM: 
+      barVis = 0 
+      frameVis = 1
  
-
-# get data from socket
+    # get data from socket
     if TCPIP:
       tcp_buffer=str(tcp_listener.buffer)
       tcp_data=tcp_buffer.replace('\000', ''); # remove null byte
       #print "%s %s" %('tcp_data', tcp_data)
 
-      if tcp_data != '\000' and tcp_data != "":
+      if tcp_data != '\000' and tcp_data != "" and tcp_data.lower() != 'nan':
         dist=float(tcp_data)
         tcp_listener.buffer="" # reset buffer
         dist_array = dist_array + [dist]
         time_array = time_array + [t]
+
         if startDetrend:
           dist_detrend = detrendDist(time_array, dist_array)
         else:
           dist_detrend = dist;
 
         fbScale = discretizeDist(dist_detrend) 
-        print 'time = %5.3f, dist = %5.3f, dist_detrend = %5.3f, fbScale = %d' % (t, dist, dist_detrend, fbScale)
+    elif TCPIP_SIM: 
+      # get dummy data from control file
+      fbScale = int(fb_array[TRcount])
+    else:
+      fbScale=0.0;
 
-    # this part of the code is executed constantly to dynamically update the sliding bar
+    # update slider bar position dynamically 
     next_bar_pos = fbScore
     if curr_bar_pos < next_bar_pos:
-      curr_bar_pos = prev_bar_pos + (next_bar_pos - prev_bar_pos)/blend_time*(t - prev_TR_time)
+      curr_bar_pos = prev_bar_pos + (next_bar_pos - prev_bar_pos)/bar_blend_time*(t - prev_TR_time)
       if curr_bar_pos > next_bar_pos:
         curr_bar_pos = next_bar_pos
     elif curr_bar_pos > next_bar_pos:
-      curr_bar_pos = prev_bar_pos + (next_bar_pos - prev_bar_pos)/blend_time*(t - prev_TR_time)
+      curr_bar_pos = prev_bar_pos + (next_bar_pos - prev_bar_pos)/bar_blend_time*(t - prev_TR_time)
       if curr_bar_pos < next_bar_pos:
         curr_bar_pos = next_bar_pos
     else:
@@ -473,17 +424,15 @@ def getState(t):
     
     bar_pos = curr_bar_pos
 
-    #print 'time = %5.3f, currBlock = %d, curr_bar_pos = %5.3f, next_bar_pos = %5.3f, bar_step = %5.3f, currBlockLen = %5.3f,fbScale = %d, ' % (t, currBlock, curr_bar_pos, next_bar_pos, bar_step, currBlockLen, fbScale) 
-    
-    return currBlock
+    return 1
 
 #############################################
 # state functions controlling presentation 
 # based on globals set in getState
 ############################################
 def myStimImage1(t):
-	global currStimTxtr
-	return currStimTxtr
+  global currStimTxtr 
+  return currStimTxtr
 
 def getBarPosition(t):
   global bar_pos, barVis, screen_half_x, screen_half_y
@@ -534,7 +483,6 @@ def getNegFbAlpha(t):
   global negFbVis, alpha_min, alpha_max
   alpha = 0
   if negFbVis == 1:
-
     alpha = alpha_min
   elif negFbVis == 2:
     alpha = alpha_max
@@ -570,13 +518,19 @@ def keydown(event):
     if event.key == pygame.locals.K_ESCAPE: 
         p.parameters.go_duration = (0, 'frames')
 
-    # Record button presses (1234)
-    if event.key == pygame.locals.K_1 or event.key == pygame.locals.K_2 or pygame.locals.K_3 or event.key == pygame.locals.K_4:
-            l_count += 1
-    elif event.key == pygame.locals.K_6 or event.key == pygame.locals.K_7 or event.key == pygame.locals.K_8 or event.key == pygame.locals.K_9:
-            r_count += 1
+    # Record button presses 1,2,3,4 as left 
+    if event.key == pygame.locals.K_1 or \
+        event.key == pygame.locals.K_2 or \
+        event.key == pygame.locals.K_3 or \
+        event.key == pygame.locals.K_4:
+      l_count += 1
 
-
+    # Record button presses 6,7,8,9 as left 
+    elif event.key == pygame.locals.K_6 or \
+        event.key == pygame.locals.K_7 or \
+        event.key == pygame.locals.K_8 or \
+        event.key == pygame.locals.K_9:
+      r_count += 1
 
 #######################
 #  Define controllers #
